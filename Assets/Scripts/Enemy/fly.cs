a99f621cd89584a8e04d18a269319e52ac960100
kamudo0102 <kamonphornudornrak@hotmail.com>
2023-10-24 10:34:14 +0200

    flies movement/prefab

--- Assets/Scripts/Enemy/fly.cs
+++ Assets/Scripts/Enemy/fly.cs
@@ -1,41 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class NewBehaviourScript : MonoBehaviour
+public class fly : MonoBehaviour
 {
-    private float xMin = -0.5f, xMax = 0.5f;
-    private float timeValue = 0.0f;
-
     public bool isMoving = true;
     // Update is called once per frame
     void Update()
     {
         if (isMoving)
         {
-         BackAndForth();
+            BackAndForth();
 
         }
         else
         {
-         bobbing();
+            Bobbing();
 
         }
     }
 
     private void BackAndForth()
     {
-        float oscillationX = transform.position.x + Mathf.Cos(Time.time * 0.23f) * 0.001f;
-        float oscillationY = transform.position.y + Mathf.Sin(Time.time + 0.5f) * 0.001f;
+        float oscillationX = transform.position.x + Mathf.Cos(Time.time * 0.25f) * 0.001f;
+        float oscillationY = transform.position.y + Mathf.Sin(Time.time + 0.3f) * 0.001f;
         transform.position = new Vector3(oscillationX, oscillationY, transform.position.z);
     }
-    private void bobbing()
+    private void Bobbing()
     {
-        float oscillationY = transform.position.y + Mathf.Sin(Time.time +5f) * 0.0005f;
+        float oscillationY = transform.position.y + Mathf.Sin(Time.time + 10f) * 0.0005f;
         transform.position = new Vector3(transform.position.x, oscillationY, transform.position.z);
     }
-
-
 }
