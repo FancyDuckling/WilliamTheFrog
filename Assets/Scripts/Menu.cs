f8fc98065f6110f93b9f9dda6b43df3924c242ad
FancyDuckling <FancyDuckling@users.noreply.github.com>
2023-10-26 16:05:55 +0200

    New idle anim and walk anim and Manue script added
    added music
    new walk frog anim
    new idle frog anim
    updated player prefab

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas creditsCanvas;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+
+        //creditsCanvas.gameObject.SetActive(true); // Enable the Canvas
+    }
+
+    public void CloseCredits()
+    {
+        SceneManager.LoadScene("Menu");
+        //creditsCanvas.gameObject.SetActive(false); // Disable the Canvas
+    }
+}
